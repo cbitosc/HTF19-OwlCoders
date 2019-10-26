0437a9642743055a4e6af53e33177e5a9bd2abc8
SatyaShodhaka <satyashodhaka66@gmail.com>
2019-10-26 08:19:25 +0530

    almost

--- grad/grad/settings.py
+++ grad/grad/settings.py
@@ -132,3 +132,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'grad/media')
 
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='peddi.vinil@gmail.com'
+EMAIL_HOST_PASSWORD='STApity123'
